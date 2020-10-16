bb5a466e69a506ca8540c3333d567615851405f0
Teri Michelle <71412924+TeriMHopkins@users.noreply.github.com>
2020-10-16 14:04:19 -0700

    add code comments

--- Project8_string_methods/js/main.js
+++ Project8_string_methods/js/main.js
@@ -3,38 +3,38 @@ function full_Sentence() {
     var two="place like ";
     var three="home ";
     var whole_sentence= one.concat(two, three);
-    document.getElementById("concatenate").innerHTML=whole_sentence;
+    document.getElementById("concatenate").innerHTML=whole_sentence; //concat method//
 }
 
 function slice_method(){
     var sentence="Some people without brains do an awful lot of talking";
     var section=sentence.slice(19,26);
-    document.getElementById("Slice").innerHTML=section;
+    document.getElementById("Slice").innerHTML=section;                    //slice method//
 }
 
 function Case_up(){
     var str="Are you a good witch or a bad witch?";
     var res=str.toUpperCase();
-    document.getElementById("witch").innerHTML=res;
+    document.getElementById("witch").innerHTML=res;    //uppercase method //
 }
 
 function string_Method(){
     var X =1939;
-    document.getElementById("release").innerHTML=("The Wizard of Oz was orignally released in ")+X.toString();
+    document.getElementById("release").innerHTML=("The Wizard of Oz was orignally released in ")+X.toString();  //string method//
 }
 
 function precision_Method() {
     var Z=26.14203200
-    document.getElementById("box_Office").innerHTML=("The films' cumulative worldwide boxoffice gross is ")+Z.toPrecision(2)+(" million dollars.");
+    document.getElementById("box_Office").innerHTML=("The films' cumulative worldwide boxoffice gross is ")+Z.toPrecision(2)+(" million dollars.");  //precision method/
 }
 
 function to_fixed() {
     var num =5.3431100;
-    document.getElementById("opening").innerHTML =("The opening week boxoffice gross for the rerelease in 1998 was ")+num.toFixed(2)+(" thousand dollars.");
+    document.getElementById("opening").innerHTML =("The opening week boxoffice gross for the rerelease in 1998 was ")+num.toFixed(2)+(" thousand dollars."); //to fixed method//
 }
 
 function val_Method() {
-    var toto="Toto, I've a feeling we're not in Kansas anymore"
+    var toto="Toto, I've a feeling we're not in Kansas anymore"  //value of method//
     var val= toto.valueOf();
     document.getElementById("kansas").innerHTML=val;
 }
